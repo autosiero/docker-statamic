253b068227691b4b739e6254587ebd8ec793ea98
Roelof Roos <github@roelof.io>
2020-10-01 16:32:35 +0200

    Added testing and compass users

--- mongodb/add-users.sh
+++ mongodb/add-users.sh
@@ -1,11 +1,9 @@
-mongo \
-    --port 27017 \
-    --authenticationDatabase 'admin' \
-    --username mongoroot \
-    --password mongoroot \
-    <<JS
+mongo --port 27017 <<JS
 // Switch to admin db
-use "admin"
+use admin
+
+// Login
+db.auth("docker-admin", "docker-admin")
 
 // Add Laravel user
 db.createUser(
@@ -13,7 +11,31 @@ db.createUser(
         user: "laravel",
         pwd: "laravel",
         roles: [
-            { role: "readWrite", db: "laravel" }
+            { role: "readWrite", db: "laravel" },
+            { role: "read", db: "laravel-testing" }
+        ]
+    }
+)
+// Add Laravel test user
+db.createUser(
+    {
+        user: "laravel-testing",
+        pwd: "laravel",
+        roles: [
+            { role: "read", db: "laravel" },
+            { role: "readWrite", db: "laravel-testing" }
+        ]
+    }
+)
+
+// Add Compass user
+db.createUser(
+    {
+        user: "compass",
+        pwd: "monitor",
+        roles: [
+            { role: "read", db: "laravel" },
+            { role: "readWrite", db: "laravel-testing" }
         ]
     }
 )
